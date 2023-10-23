240a79d680ae5f9ffd7de7f097308f2d0d3ee38e
Bill3710 <Bill3710@server.ca>
2023-10-23 13:18:17 -0700

    update header of the file table

--- kern/include/file_table.h
+++ kern/include/file_table.h
@@ -6,5 +6,27 @@
 #include <limits.h>
 #include <vnode.h>
 
-struct file_table {};
+struct open_file { 
+    struct vnode *vn;
+    int flag;
+    int refcount;
+    off_t offset;
+    struct lock *flock;
+} open_file;
+
+struct file_table {
+    struct open_file *table[OPEN_MAX];
+	struct lock *file_table_lock;
+} file_table;
+
+// function for open_file
+struct open_file * of_create (struct vnode *vn, int flag);
+void of_destroy(struct open_file *of);
+void open_file_incref(struct open_file *of);
+void open_file_decref(struct open_file *of);
+
+//function for file table
+struct file_table * ft_create (void);
+void ft_destroy(struct file_table *ft);
+int ft_init (filetable *ft);
 #endif
