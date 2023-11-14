358e564ab3193526606597bed847a99d9c17744f
Bill3710 <Bill3710@server.ca>
2023-11-14 13:30:15 -0800

    keep work on compile

--- kern/include/proc_syscalls.h
+++ kern/include/proc_syscalls.h
@@ -10,7 +10,7 @@ void child_entry_point(void *trapframe, unsigned long data2)
 int execv(const char *program, char **args);
 int check_argument_validity_execv(char ** args);
 void free_arg_pointers(char **arg_pointers, int end_index);
-void revert_as(struct *addrspace old_as, struct *addrspace new_as);
+void revert_as(struct addrspace *old_as, struct addrspace *new_as);
 int waitpid(int pid, userptr_t status, int options, int *retval);
 int _exit(int exitcode);
 int getpid(int *retval);
