030a07f595cf75baec0d0ed317ac0344031f2909
Bill3710 <Bill3710@server.ca>
2023-12-01 15:55:21 -0800

    coremap

---
+++ kern/include/coremap.h
@@ -0,0 +1,17 @@
+#include <types.h>
+#include <spinlock.h>
+#include <machine/vm.h>
+#include <addrspace.h>
+
+bool ready = false;
+struct cm_entry *coremap;
+paddr_t cm_paddr;
+static struct spinlock cm_spinlock = SPINLOCK_INITIALIZER;
+int total_pages;
+
+struct cm_entry {
+    vaddr_t page_vaddr;
+    int num_pages;
+    bool ALLOCATE;
+} cm_entry;
+
